834adf4cef4d510c3a94716a7c9877dd410f41a2
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-01-22 19:45:49 +0100

    start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import './css/styles.css';
+import { fetchPhotos } from "./js/fetchPhotos";
+import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const DEBOUNCE_DELAY = 3;
+const form = document.querySelector('#search-form');
+const input = document.querySelector('input');
+const divGallary = document.querySelector('gallery');
+
+
+
+form.addEventListener('submit', onSubmitForm);
+divGallary.addEventListener('click', onClickCard);
---
+++ src/js/fetchPhotos.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const BAZE_URL = 'https://pixabay.com/api/';
+const KEY = '33040726-8ad8a9d3351d63d14c4afe724';
+
+export async function fetchPhotos(){
+  const response = await axios.get(
+    `${BAZE_URL}/?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=ture&per_page=40&page=${page}`);
+  
+  return response.data;
+}
+
