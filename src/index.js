6717e7d6d50a6e992eabefebd8a7e5ca3d129865
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-01-26 07:11:02 +0100

    +-

--- src/index.js
+++ src/index.js
@@ -3,11 +3,267 @@ import Notiflix from "notiflix";
 import SimpleLightbox from "simplelightbox";
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// import { pageScrolling } from './js/pageScroll';
+
+// let page = 1;
+// let gallery = new SimpleLightbox('div.gallery a', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
+
+// const form = document.querySelector('#search-form');
+// const divGallary = document.querySelector('.gallery');
+// const input = document.querySelector('input');
+// const divGuard = document.querySelector('.js-guard');
+
+// form.addEventListener('submit', onSubmitForm);
+// input.addEventListener('input', onInput);
+
+// function onSubmitForm(e) {
+//   e.preventDefault();
+//   page = 1;
+//   getData(input.value, page);
+// }
+
+// function onInput(e) {
+//   if (e.target.value) {
+//     divGallary.innerHTML = '';
+//   }
+// }
+
+// async function getData(query, page) {
+//   try {
+//     const data = await fetchPhotos(query, page);
+//     // console.log(data);
+//     renderImages(data);
+//     gallery.refresh();
+//     observer.observe(divGuard);
+//     return data;
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// }
+
+// function renderImages({ data: { hits: arrOfImages }, data: { totalHits } }) {
+//   notifixForms(arrOfImages, totalHits);
+
+//   const markup = arrOfImages
+//     .map(image => {
+//       const {
+//         webformatURL,
+//         largeImageURL,
+//         tags,
+//         likes,
+//         views,
+//         comments,
+//         downloads,
+//       } = image;
+
+//       console.log(arrOfImages);
+
+//       return `<div class="photo-card">
+//   <a href="${largeImageURL}" class="gallery__item"><img src="${webformatURL}" alt="${tags}" class="gallery__image"  loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes</b>
+//       ${likes}
+//     </p>
+//     <p class="info-item">
+//       <b>Views</b>
+//       ${views}
+//     </p>
+//     <p class="info-item">
+//       <b>Comments</b>
+//       ${comments}
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads</b>
+//       ${downloads}
+//     </p>
+//   </div>
+// </div></a>`;
+//     })
+//     .join('');
+
+//     divGallary.insertAdjacentHTML('beforeend', markup);
+//   markup.refresh();
+// }
+
+// function notifixForms(arrOfImages, totalHits) {
+//   if (arrOfImages.length === 0) {
+//     return Notiflix.Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//   }
+
+//   if (page > totalHits / 40 && arrOfImages.length !== 0) {
+//     return Notiflix.Notify.info(
+//       "We're sorry, but you've reached the end of search results."
+//     );
+//   }
+
+//   if (arrOfImages.length !== 0) {
+//     console.log(page);
+//     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+//   }
+// }
+
+// const options = {
+//   root: null,
+//   rootMargin: '250px',
+//   threshold: 1.0,
+// };
+
+// let observer = new IntersectionObserver(onLoad, options);
+
+// function onLoad(entries, observer) {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting === true) {
+//       page += 1;
+//       getData(input.value, page).then(data => {
+//         const {
+//           data: { hits },
+//         } = data;
+
+//         if (page === 13 || hits.length < 40) {
+//           observer.unobserve(divGuard);
+//           return Notiflix.Notify.info(
+//             "We're sorry, but you've reached the end of search results.");
+//         }
+//       });
+//     }
+//   });
+// }
+
+
+
+
+
+
+
+
+
+let page = 1;
+let gallery = new SimpleLightbox('div.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 500,
+});
+
 const form = document.querySelector('#search-form');
+const div = document.querySelector('.gallery');
 const input = document.querySelector('input');
-const divGallary = document.querySelector('gallery');
-
-
+const divGuard = document.querySelector('.js-guard');
 
 form.addEventListener('submit', onSubmitForm);
-divGallary.addEventListener('click', onClickCard);
+input.addEventListener('input', onInput);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  page = 1;
+  getData(input.value, page);
+}
+
+function onInput(e) {
+  if (e.target.value) {
+    div.innerHTML = '';
+  }
+}
+
+async function getData(query, page) {
+  try {
+    const data = await fetchPhotos(query, page);
+    renderImages(data);
+    gallery.refresh();
+    observer.observe(divGuard);
+    return data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function renderImages({ data: { hits: arrOfImages }, data: { totalHits } }) {
+  notifixForMessage(arrOfImages, totalHits);
+
+  const markup = arrOfImages
+    .map(image => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+
+      return `<div class="photo-card">
+  <a href="${largeImageURL}" class="gallery__item"><img src="${webformatURL}" alt="${tags}" class="gallery__link"  loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      ${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      ${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      ${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      ${downloads}
+    </p>
+  </div>
+</div></a>`;
+    })
+    .join('');
+
+  div.insertAdjacentHTML('beforeend', markup);
+}
+
+
+function notifixForMessage(arrOfImages, totalHits) {
+  if (page === 1 && arrOfImages.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  if (page > totalHits / 40 && arrOfImages.length !== 0) {
+    Notiflix.Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  if (page === 1 && arrOfImages.length !== 0) {
+    console.log(page);
+    Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+  }
+}
+
+const options = {
+  root: null,
+  rootMargin: '250px',
+  threshold: 1.0,
+};
+
+let observer = new IntersectionObserver(onLoad, options);
+
+function onLoad(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      getData(input.value, page).then(data => {
+        const {
+          data: { hits },
+        } = data;
+
+        if (page === 13 || hits.length < 40) {
+          observer.unobserve(divGuard);
+        }
+      });
+    }
+  });
+}
